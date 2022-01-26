585104daa229609e3531f6a93c8a1511d68c831f
topias <topias@Topias-PC>
2022-01-27 00:02:08 +0200

    Kaikkea kivaa

--- TopStock-Java/src/fi/mariapori/topstock/Paaikkuna.java
+++ TopStock-Java/src/fi/mariapori/topstock/Paaikkuna.java
@@ -21,6 +21,13 @@ import javax.swing.JLabel;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JSpinner;
+import java.awt.Font;
+import javax.swing.JScrollBar;
+import javax.swing.JMenuBar;
 
 public class Paaikkuna extends JFrame {
 
@@ -62,7 +69,13 @@ public class Paaikkuna extends JFrame {
 	public Paaikkuna(ConnectionSource connectionSource) {
 		setTitle("TopStock");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 321);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JButton btnNewButton = new JButton("Uusi tuote");
+		menuBar.add(btnNewButton);
 		contentPane = new JPanel();
 
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -70,19 +83,19 @@ public class Paaikkuna extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Tuotteet");
-		lblNewLabel.setBounds(48, 12, 70, 15);
+		lblNewLabel.setBounds(53, 33, 70, 25);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblTuotekoodi = new JLabel("");
-		lblTuotekoodi.setBounds(277, 32, 70, 15);
+		lblTuotekoodi.setBounds(277, 32, 135, 15);
 		contentPane.add(lblTuotekoodi);
 		
 		JLabel lblTuotenimi = new JLabel("");
-		lblTuotenimi.setBounds(277, 56, 70, 15);
+		lblTuotenimi.setBounds(277, 56, 135, 15);
 		contentPane.add(lblTuotenimi);
 		
 		JLabel lblSaldo = new JLabel("");
-		lblSaldo.setBounds(277, 77, 70, 15);
+		lblSaldo.setBounds(277, 77, 135, 15);
 		contentPane.add(lblSaldo);
 		
 		JList<String> list = new JList<String>(listaTuotteet);
@@ -97,8 +110,81 @@ public class Paaikkuna extends JFrame {
 			});
 		list.setVisibleRowCount(-1);
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list.setBounds(12, 33, 154, 226);
+		list.setBounds(12, 56, 154, 203);
+		
 		contentPane.add(list);
+		
+		JSpinner saldoSpinner = new JSpinner();
+		saldoSpinner.setFont(new Font("Dialog", Font.BOLD, 24));
+		saldoSpinner.setBounds(368, 172, 70, 87);
+		contentPane.add(saldoSpinner);
+		
+		JButton btnLisaavarastoon = new JButton("Lisää varastoon");
+		btnLisaavarastoon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int index = list.getSelectedIndex();
+				Tavara tavara = tuotteet.get(index);
+				tavara.AddSaldo((Double.valueOf((int)saldoSpinner.getValue())));
+				try {
+					Dao<Tavara, String> productDao = DaoManager.createDao(connectionSource, Tavara.class);
+					productDao.update(tavara);
+					productDao.refresh(tavara);
+					lblSaldo.setText("Saldo: " + tavara.getSaldo());
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					tavara.RemoveSaldo(Double.valueOf((int)saldoSpinner.getValue()));
+				}
+				saldoSpinner.setValue(0);
+			}
+		});
+		btnLisaavarastoon.setBounds(178, 162, 169, 25);
+		contentPane.add(btnLisaavarastoon);
+		
+		JButton btnPoistavarastosta = new JButton("Poista varastosta");
+		btnPoistavarastosta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int index = list.getSelectedIndex();
+				Tavara tavara = tuotteet.get(index);
+				tavara.RemoveSaldo((Double.valueOf((int)saldoSpinner.getValue())));
+				try {
+					Dao<Tavara, String> productDao = DaoManager.createDao(connectionSource, Tavara.class);
+					productDao.update(tavara);
+					productDao.refresh(tavara);
+					lblSaldo.setText("Saldo: " + tavara.getSaldo());
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					tavara.AddSaldo(Double.valueOf((int)saldoSpinner.getValue()));
+				}
+				saldoSpinner.setValue(0);
+			}
+		});
+		btnPoistavarastosta.setBounds(178, 199, 169, 25);
+		contentPane.add(btnPoistavarastosta);
+		
+		JButton btnNollaa = new JButton("Nollaa");
+		btnNollaa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double vanhaSaldo = 0;
+				int index = list.getSelectedIndex();
+				Tavara tavara = tuotteet.get(index);
+				vanhaSaldo = tavara.getSaldo();
+				tavara.ClearSaldo();
+				try {
+					Dao<Tavara, String> productDao = DaoManager.createDao(connectionSource, Tavara.class);
+					productDao.update(tavara);
+					productDao.refresh(tavara);
+					lblSaldo.setText("Saldo: " + tavara.getSaldo());
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					tavara.AddSaldo(vanhaSaldo);
+				}
+				saldoSpinner.setValue(0);
+			}
+		});
+		btnNollaa.setBounds(178, 234, 169, 25);
+		contentPane.add(btnNollaa);
+		
+
 		
 	}
 	
